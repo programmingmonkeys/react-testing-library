d1592754f1919eb80a5afa1c2f3571599f804544
codingChewie <codingChewie@gmail.com>
2021-01-05 17:16:37 -0600

    increase and decrease

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
   return (
     <div className="App">
-      <button>Increase</button>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrease</button>
+      <h3>{counter}</h3>
     </div>
   )
 }
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,7 +1,17 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from '../App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const increaseBtn = screen.getByText(/increase/i)
+  const increaseBtn = screen.getByText(/increase/i) // regex
+  const decreaseBtn = screen.getByText('Decrease') // string
+
+  const counter = screen.getByText(0)
+
+  userEvent.click(increaseBtn)
+  expect(counter).toHaveTextContent(1)
+
+  userEvent.click(decreaseBtn)
+  expect(counter).toHaveTextContent(0)
 })
